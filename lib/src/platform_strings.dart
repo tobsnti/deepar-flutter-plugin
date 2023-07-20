efb6f0ae6dadfa1a8457bb8480ae87388b2facc9
tobsnti <hello@tobiastimm.de>
2023-07-20 13:15:21 +0200

    use cocoapods package to install DeepAR

--- lib/src/platform_strings.dart
+++ lib/src/platform_strings.dart
@@ -7,6 +7,7 @@ class PlatformStrings {
   // Methods
   static const String initialize = "initialize";
   static const String switchEffect = "switch_effect";
+  static const String backgroundReplacement = "background_replacement";
   static const String startCamera = "start_camera";
   static const String getResolution = "get_resolution";
   static const String startRecordingVideo = "start_recording_video";
@@ -17,5 +18,6 @@ class PlatformStrings {
   // Method arguments
   static const String licenseKey = "license_key";
   static const String effect = "effect";
+  static const String image = "image";
   static const String resolution = "resolution";
 }
